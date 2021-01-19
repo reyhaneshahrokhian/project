e73d4c6b09a52c92dbec471e32a5d2a551bf1d56
reyhaneshahrokhian <r_shahrokhian@comp.iust.ac.ir>
2021-01-19 17:06:18 +0330

    make the food grass and rock move

--- main.cpp
+++ main.cpp
@@ -33,12 +33,16 @@ int main()
     Sprite spriteGrasmove1;
     spriteGrasmove1.setTexture(textureGrasmove1);
     spriteGrasmove1.setPosition(100,0);
+    bool grass1=false;
+    float grass1Speed=0.0f;
       
     Texture textureGrasmove2;
     textureGrasmove2.loadFromFile("project image/grass move.jpg");
     Sprite spriteGrasmove2;
     spriteGrasmove2.setTexture(textureGrasmove2);
     spriteGrasmove2.setPosition(510,0);
+    bool grass2=false;
+    float grass2Speed=0.0f;
 
     //shark
     Texture textureShark;
@@ -53,6 +57,8 @@ int main()
     Sprite spriteFood;
     spriteFood.setTexture(textureFood);
     spriteFood.setPosition(300,100);
+    bool food=false;
+    float foodSpeed=50.0f;
 
     //shooter
     Texture textureShooter;
@@ -66,13 +72,88 @@ int main()
     textureRock.loadFromFile("project image/rock.png");
     Sprite spriteRock;
     spriteRock.setTexture(textureRock);
-    spriteRock.setPosition(400,100);    
+    spriteRock.setPosition(400,100);   
+    bool rock=false;
+    float rockSpeed=50.0f;
+
+    Clock clock; 
 
     while (window.isOpen()){
         
         
         if(Keyboard::isKeyPressed(Keyboard::Escape)){
             window.close();
+        }
+
+        Time dt=clock.restart();
+        
+        if(grass1==false){
+            //grass1 speed
+            srand(time(NULL)*2);
+            grass1Speed = (rand()%50+50);
+            spriteGrasmove1.setPosition(100,-200);
+            grass1=true;
+        }else{
+            //moving grass1
+            spriteGrasmove1.setPosition(
+            100,spriteGrasmove1.getPosition().y+(grass1Speed*dt.asSeconds()));
+            
+            if(spriteGrasmove1.getPosition().y >810){
+                grass1=false;
+            }
+        }
+
+        if(!grass2){
+            //grass2 speed
+            srand(time(NULL)*6);
+            grass2Speed = (rand()%50+50);
+            spriteGrasmove2.setPosition(510,-200);
+            grass2=true;
+        }else{
+            //moving grass2
+            spriteGrasmove2.setPosition(
+                510,spriteGrasmove2.getPosition().y+(grass2Speed * dt.asSeconds()));
+        
+            if(spriteGrasmove2.getPosition().y >810){
+                grass2=false;
+            }
+        }
+
+        if(!food){
+            //x food
+            srand(time(NULL)*3);
+            float x=(rand()%600)+101;
+            spriteFood.setPosition(x,-40);
+            food=true;
+        }else{
+            //moving food
+            spriteFood.setPosition(
+            spriteFood.getPosition().x,    
+            spriteFood.getPosition().y+(foodSpeed * dt.asSeconds()));
+        
+            if(spriteFood.getPosition().y >810){
+                food=false;
+            }
+        }
+
+        if(!rock){
+            //x rock
+            srand(time(NULL)*3);
+            float xr;
+            do{
+            float xr=(rand()%600)+101;
+            }while(spriteFood.getPosition().x==xr);
+            spriteRock.setPosition(xr,-60);
+            rock=true;
+        }else{
+            //moving rock
+            spriteRock.setPosition(
+            spriteRock.getPosition().x,    
+            spriteRock.getPosition().y+(rockSpeed * dt.asSeconds()));
+        
+            if(spriteRock.getPosition().y >810){
+                rock=false;
+            }
         }
 
         window.clear();
